698b25b343855ac6818641138f574fd2c43dba7c
nana-yyy <95006305+nana-yyy@users.noreply.github.com>
2021-12-09 23:13:54 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -30,7 +30,7 @@ if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   let storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+  myHeading.innerHTML = 'こんにちは, ' + storedName;
 }
 
 myButton.onclick = function() {
