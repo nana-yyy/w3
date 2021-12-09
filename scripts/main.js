4d017ce684e0709cf22530c64cc376d2bfcacf00
nana-yyy <95006305+nana-yyy@users.noreply.github.com>
2021-12-09 23:06:54 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -17,7 +17,7 @@ let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
 function setUserName() {
-  let myName = prompt('Please enter your name.');
+  let myName = prompt('名前を入力してください');
   if(!myName) {
     setUserName();
   } else {
